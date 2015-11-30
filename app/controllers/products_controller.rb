86ff3f693a96c4ebbc8279070454f2dcf7f1a81e
Hershel Nashman <hershel.nashman@gmail.com>
2015-11-30 16:23:31 -0500

    AJAX search

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,17 @@
 class ProductsController < ApplicationController
   def index
-  	@products = Product.all
+  	@products = if params[:search]
+      Product.where("LOWER(name) LIKE LOWER(?)", "%#{params[:search]}")
+    else
+      Product.all
+    end
+
+    if request.xhr?
+    render @products
+    end
   end
+
+
 
   def show
   	@product = Product.find(params[:id])
